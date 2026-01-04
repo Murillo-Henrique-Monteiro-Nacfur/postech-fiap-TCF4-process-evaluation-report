a37c5498725122bbbbf5f90e9b0850397781fabf
Murillo-Henrique-Monteiro-Nacfur <53658401+Murillo-Henrique-Monteiro-Nacfur@users.noreply.github.com>
2026-01-04 18:26:04 -0300

    Update GenerateReportUseCase.java

--- src/main/java/com/postech/fiap/usecase/GenerateReportUseCase.java
+++ src/main/java/com/postech/fiap/usecase/GenerateReportUseCase.java
@@ -66,7 +66,7 @@ public class GenerateReportUseCase {
         var generateCSV = generateCsvReportService.generateCSV(evaluationsInLastWeek);
         var emailMessage = generateEmailReportService.generateReport(evaluationsInLastWeek);
         var bucketName = "report-bucket-techfiap";
-        var fileName = "Report-" + LocalDateTime.now();
+        var fileName = "Report-" + LocalDateTime.now() + ".csv";
         var objectName = gcsUploadService.uploadFile(bucketName, fileName, generateCSV, "report.csv");
         var emailsToSend = getEmailsToSendReport();
 
